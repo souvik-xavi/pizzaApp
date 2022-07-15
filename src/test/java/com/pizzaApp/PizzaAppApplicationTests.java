0ed463232319dccca63b3c1f2d4ae100fd7b33b1
souvik-xavi <souvikghata2@gmail.com>
2022-07-15 16:16:42 +0530

    Login and Registration Test cases

--- src/test/java/com/pizzaApp/PizzaAppApplicationTests.java
+++ src/test/java/com/pizzaApp/PizzaAppApplicationTests.java
@@ -1,13 +1,42 @@
 package com.pizzaApp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.pizzaApp.Entity.Customer;
+import com.pizzaApp.Entity.User;
+import com.pizzaApp.Repository.CustomerRepository;
+import com.pizzaApp.Services.LoginServices;
 
 @SpringBootTest
 class PizzaAppApplicationTests {
 
+	@Autowired
+	CustomerRepository cRepo;
+	@Autowired
+	LoginServices loginservices;
 	@Test
-	void contextLoads() {
+	public void test() {
+		Customer c= new Customer("souvik","souvikghata21@","souvik");
+		cRepo.save(c);
+		assertNotNull(cRepo.findAll());
+
+	}
+
+	@Test
+	public void testLogin() {
+		Customer c= new Customer("souvik","souvikghata21@","souvik");
+		cRepo.save(c);
+		User u=new User("souvikghata21@","souvik");
+		User uf=new User("souvikghata21@","/souvik");
+		String q=loginservices.login(u);
+		assertEquals("accepted", q);
+		String q1=loginservices.login(uf);
+		assertEquals("declined", q1);
 	}
 
 }
