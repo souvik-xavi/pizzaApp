431f6eeb6dc7aaeb0f2ac12156724475948db82d
Sauptik2000 <sauptikghosh999@gmail.com>
2022-07-20 14:30:51 +0530

    fix: fe & be

--- fe/src/components/Pizza.js
+++ fe/src/components/Pizza.js
@@ -7,7 +7,7 @@ const Pizza = () => {
     const viewPizza = async ()=>{
       try {
         const res = await axios.get(`http://localhost:8080/viewPizza`);
-        console.log(res);
+        console.log(res.data);
       } catch (error) {
         console.log("Error Found")
       }
